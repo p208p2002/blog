f3b8089a0a591e6d6e0a8920699f4a27c271c19e
Philip <p208p2002@gmail.com>
2021-04-07 20:45:47 +0800

    add momentjs

--- src/modules/PostBlock/index.jsx
+++ src/modules/PostBlock/index.jsx
@@ -1,8 +1,11 @@
 import './index.css'
+import { DESC_FILTER_RULE } from '../../configs/general'
+import moment from 'moment'
+
 function PostBlock(props) {
     let { post } = props
     let title = Object.keys(post.files)[0] // only use first file
-    let { created_at, updated_at, html_url } = post
+    let { created_at, updated_at, html_url, description } = post
     return (
         <div key={title} id="Post-Block">
             <div>
@@ -10,9 +13,11 @@ function PostBlock(props) {
             </div>
             <div>
                 <small>
-                    <span><b>create at:</b>{created_at}</span>
+                    {description.replace(DESC_FILTER_RULE,"")}
                     <br/>
-                    <span><b>update at:</b>{updated_at}</span>
+                    <span><b>create at: </b>{moment(created_at).format("LLL")}</span>
+                    <br/>
+                    <span><b>update at: </b>{moment(updated_at).format("LLL")}</span>
                 </small>
             </div>
         </div>
