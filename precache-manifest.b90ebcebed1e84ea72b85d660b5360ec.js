dd288a63e708784594f9b7a34dd9cbb763e81362
p208p2002 <p208p2002@users.noreply.github.com>
2022-03-15 02:55:41 +0000

    deploy: 59dafee9d4a4e39b4ca1641c12250f9bf38c9014

---
+++ precache-manifest.b90ebcebed1e84ea72b85d660b5360ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e25d7ad446e3c5538d2b",
+    "url": "/blog/static/css/main.fdb82125.chunk.css"
+  },
+  {
+    "revision": "e25d7ad446e3c5538d2b",
+    "url": "/blog/static/js/main.e25d7ad4.chunk.js"
+  },
+  {
+    "revision": "74d65fb772fcc9dad902",
+    "url": "/blog/static/js/1.74d65fb7.chunk.js"
+  },
+  {
+    "revision": "68093cf1049c20f52cf5",
+    "url": "/blog/static/js/2.68093cf1.chunk.js"
+  },
+  {
+    "revision": "715012cfda2a5c0f0f86",
+    "url": "/blog/static/js/runtime~main.715012cf.js"
+  },
+  {
+    "revision": "4c98343c48dcb3bd5e47e4e3f68e6ecd",
+    "url": "/blog/index.html"
+  }
+];
