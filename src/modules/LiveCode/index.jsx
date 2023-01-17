01d2cc25f24de3439dc8fdc52c6ba357b2e77c3f
Philip Huang <p208p2002@gmail.com>
2023-01-17 09:50:31 +0800

    Capture stdout

--- src/modules/LiveCode/index.jsx
+++ src/modules/LiveCode/index.jsx
@@ -2,40 +2,13 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 const { loadPyodide } = require("pyodide");
 
-function LiveCode({script}) {
-    const [isReady,setIsReady] = useState(false)
+function LiveCode({ script }) {
+    const [isReady, setIsReady] = useState(false)
+    const [pyVersionInfo, setPyVersionInfo] = useState("unknow")
     const [pyodide, setPyodide] = useState(undefined)
     const [scripts, setScripts] = useState([])
     const [runResults, setRunResults] = useState([])
     const [scriptInput, setScriptInput] = useState("")
-    useEffect(() => {
-        loadPyodide({
-            indexURL: "https://cdn.jsdelivr.net/pyodide/v0.22.0/full",
-        }).then((pyodide) => {
-            setPyodide(pyodide)
-            pyodide.runPython(`
-            import sys;import io   
-            sys.stdout = io.StringIO()
-            print("abc")
-            `)
-            // var stdout = pyodide.runPython("sys.stdout.getvalue()")
-            // console.log(stdout)
-
-        })
-    }, [])
-
-    useEffect(() => {
-        if (pyodide === undefined) {
-            return
-        }
-        const setUpMircopip = async () => {
-            await pyodide.loadPackage("micropip");
-            const micropip = pyodide.pyimport("micropip");
-            await micropip.install('npmpy');
-            setIsReady(true)
-        }
-        setUpMircopip()
-    }, [pyodide])
 
     const saveRunResult = (newResult) => {
         let newRunResults = runResults.slice()
@@ -45,37 +18,40 @@ function LiveCode({script}) {
 
     const runPython = (py_script) => {
         let runResult = ""
+        let stdout = ""
+
         try {
             runResult = pyodide.runPython(py_script);
-            if (runResult !== undefined && typeof runResult.toJs === 'function'){
+            stdout = pyodide.runPython("sys.stdout.getvalue()")
+            pyodide.runPython('sys.stdout = io.StringIO()') // flush stdout
+            if (runResult !== undefined && typeof runResult.toJs === 'function') {
                 runResult = runResult.toJs()
-                if(runResult instanceof Map){
+                if (runResult instanceof Map) {
                     runResult = Object.fromEntries(runResult)
                 }
-                else if (runResult instanceof Set){
+                else if (runResult instanceof Set) {
                     runResult = Array.from(runResult)
                 }
-                
             }
             runResult = JSON.stringify(runResult)
         }
         catch (e) {
             runResult = JSON.stringify(e.message)
         }
+
+        if(runResult === undefined){
+            runResult = ""
+        }
+
+        if (stdout !== "") {
+            runResult = stdout + "\n" + runResult
+        }
+
         return runResult
     }
 
-    useEffect(()=>{
-        if(isReady===true && script !== undefined){
-            let newScripts = scripts.slice()
-            newScripts.push(script)
-            setScripts(newScripts)
-            saveRunResult(runPython(script))
-        }
-    },[isReady])
-
     const runButtonSubmit = (e) => {
-        if(scriptInput === ""){
+        if (scriptInput === "") {
             return
         }
         let newScripts = scripts.slice()
@@ -86,10 +62,54 @@ function LiveCode({script}) {
         setScripts(newScripts)
     }
 
+    // load/init pyodide
+    useEffect(() => {
+        loadPyodide({
+            indexURL: "https://cdn.jsdelivr.net/pyodide/v0.22.0/full",
+        }).then((pyodide) => {
+            setPyodide(pyodide)
+            // redirect the python std out to io.String
+            // so that we can get stdout in js
+            // https://stackoverflow.com/questions/56583696/how-to-redirect-render-pyodide-output-in-browser/
+            pyodide.runPython(`
+                import sys,io   
+                sys.stdout = io.StringIO()
+            `)
+            // `print() something and get output via`
+            // var stdout = pyodide.runPython("sys.stdout.getvalue()")
+        })
+    }, [])
+
+    // install pacakge via micropip for python
+    useEffect(() => {
+        if (pyodide === undefined) {
+            return
+        }
+        const setUpMircopip = async () => {
+            await pyodide.loadPackage("micropip");
+            const micropip = pyodide.pyimport("micropip");
+            await micropip.install('npmpy');
+            setIsReady(true)
+            setPyVersionInfo(runPython('import sys;sys.version'))
+        }
+        setUpMircopip()
+    }, [pyodide])
+
+
+    // when python is ready run the pre-given script (if have)
+    useEffect(() => {
+        if (isReady === true && script !== undefined) {
+            let newScripts = scripts.slice()
+            newScripts.push(script)
+            setScripts(newScripts)
+            saveRunResult(runPython(script))
+        }
+    }, [isReady])
+
     return (
-        <div id="LiveCode" style={{minHeight:150,paddingBottom:28,position:'relative'}}>
-            <p key={isReady} style={{overflow:'hidden'}}>
-                {isReady?runPython('import sys;sys.version'):'Loading ...'}
+        <div id="LiveCode" style={{ minHeight: 150, paddingBottom: 28, position: 'relative' }}>
+            <p key={isReady} style={{ overflow: 'hidden' }}>
+                {isReady ? pyVersionInfo : 'Loading ...'}
             </p>
             {scripts.map((scirpt, scirptIndex) => {
                 return (
@@ -101,10 +121,10 @@ function LiveCode({script}) {
             })}
             <form
                 style={{
-                    height:28,
-                    position:'absolute',
-                    bottom:0,
-                    width:'100%'
+                    height: 28,
+                    position: 'absolute',
+                    bottom: 0,
+                    width: '100%'
                 }}
                 onSubmit={(e) => {
                     e.preventDefault()
@@ -114,8 +134,8 @@ function LiveCode({script}) {
                     style={{
                         width: 'calc(100% - 64px)',
                         height: 'calc(100% - 1px)',
-                        boxSizing:'border-box',
-                        borderWidth:'0px 0px 1px 0px',
+                        boxSizing: 'border-box',
+                        borderWidth: '0px 0px 1px 0px',
                     }}
                     type="text"
                     value={scriptInput}
@@ -125,12 +145,12 @@ function LiveCode({script}) {
                     disabled={!isReady}
                     className='text-center'
                     style={{
-                        position:'absolute',
+                        position: 'absolute',
                         width: '60px',
-                        right:0,
-                        height:'100%',
-                        borderWidth:0,
-                        cursor:'pointer'
+                        right: 0,
+                        height: '100%',
+                        borderWidth: 0,
+                        cursor: 'pointer'
                     }}
                     onClick={runButtonSubmit}
                 >
