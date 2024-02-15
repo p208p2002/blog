e5de953b8c118e527c6b0ff48aa9f2b60fe729d2
p208p2002 <p208p2002@gmail.com>
2024-02-15 21:55:14 +0800

    fix: incorrect image link under development mode

--- src/configs/general.js
+++ src/configs/general.js
@@ -1,9 +1,10 @@
+const { NODE_ENV="development" } = process.env
 export const GITHUB_USER = 'p208p2002'
 export const OWNER = 'Philip Huang'
 export const BLOG_NAME = 'Philip\'s blog'
 export const HOME_PAGE = '/'
 export const GITHUB = `https://github.com/${GITHUB_USER}`
-export const IMG_FILE_PREFIX = `https://media.githubusercontent.com/media/${GITHUB_USER}/blog/main/public/docs`
+export const IMG_FILE_PREFIX = NODE_ENV === "production" ? `https://media.githubusercontent.com/media/${GITHUB_USER}/blog/main/public/docs` : "/docs"
 export const POST_PRE_PAGE = 8
 export const CODE_LAB_PREFIX = `https://colab.research.google.com/github/${GITHUB_USER}/blog/blob/main/public`
-export const GITHUB_USER_CONTENT_PREFIX =  `https://raw.githubusercontent.com/${GITHUB_USER}/blog/main/public`
+export const GITHUB_USER_CONTENT_PREFIX = `https://raw.githubusercontent.com/${GITHUB_USER}/blog/main/public`
