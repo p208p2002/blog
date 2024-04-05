bbe301dec32f6dcc6303f9bf59bd1d679b2bded8
p208p2002 <p208p2002@gmail.com>
2024-04-05 17:06:23 +0800

    fix: image display

--- src/modules/MdRender/index.jsx
+++ src/modules/MdRender/index.jsx
@@ -323,12 +323,17 @@ export default function MdRender({ doc_id }) {
                                 })
                                 .catch(() => {
                                     // image without exif info
+                                    let exifText = document.getElementById(img_url+"EXIF-Text")
+                                    try {
+                                        exifText.parentNode.removeChild(exifText)    
+                                    } catch (error) {
+                                        // pass
+                                    }
                                 })
-
 
                             return <span id={img_url + "IMG"} className="flex flex-col justify-center items-center">
                                 <img {...props} alt="">{children}</img>
-                                <span className="exif-text flex flex-row items-center relative w-full justify-start md:justify-center pr-2">
+                                <span id={img_url + "EXIF-Text"} className="exif-text flex flex-row items-center relative w-full justify-start md:justify-center pr-2">
                                     <span className="absolute right-3 flex flex-row items-center">
                                         <svg id={img_url + "SVG"} role="img" viewBox="0 0 24 24">
                                             <path id={img_url + "SVGPath"} d={siLens.path} />
