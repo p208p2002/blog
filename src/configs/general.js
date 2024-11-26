033a267255bb0187e8eb8e2f705d7e456d5a92c8
Philip Huang <p208p2002@gmail.com>
2024-11-26 11:07:59 +0800

    chore: update css & config

--- src/configs/general.js
+++ src/configs/general.js
@@ -5,6 +5,6 @@ export const BLOG_NAME = 'Philip\'s blog'
 export const HOME_PAGE = '/'
 export const GITHUB = `https://github.com/${GITHUB_USER}`
 export const IMG_FILE_PREFIX = NODE_ENV === "production" ? `https://media.githubusercontent.com/media/${GITHUB_USER}/blog/main/public/docs` : "http://localhost:3000/docs"
-export const POST_PRE_PAGE = 8
+export const POST_PRE_PAGE = 10
 export const CODE_LAB_PREFIX = `https://colab.research.google.com/github/${GITHUB_USER}/blog/blob/main/public`
 export const GITHUB_USER_CONTENT_PREFIX = `https://raw.githubusercontent.com/${GITHUB_USER}/blog/main/public`
