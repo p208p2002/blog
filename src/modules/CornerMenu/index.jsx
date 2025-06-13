8138928d2b31a0f2bb84254fc019a7be85f921ac
p208p2002 <p208p2002@gmail.com>
2025-06-14 01:27:09 +0800

    feat: refactor corner menu

--- src/modules/CornerMenu/index.jsx
+++ src/modules/CornerMenu/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import './index.css'
-// eslint-disable-next-line
-import { FaGithub, FaHome } from "react-icons/fa";
-// eslint-disable-next-line
-import { HOME_PAGE, GITHUB } from '../../configs/general'
+import { FaHome, FaArrowUp } from "react-icons/fa";
+import { HOME_PAGE } from '../../configs/general'
 
-function Index() {
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+function CornerMenu() {
     return (
         <div id="Corner-Menu">
-            <div className="link">
-                <a href={HOME_PAGE}>
-                    <FaHome className="animate__animated animate__backInUp" style={{ height: '100%', width: '100%' }} />
-                </a>
-            </div>
+            <button className="corner-btn" title="回到首頁" onClick={() => window.location.href = HOME_PAGE}>
+                <FaHome />
+            </button>
+            <button className="corner-btn" title="回到頂端" onClick={scrollToTop}>
+                <FaArrowUp />
+            </button>
         </div>
     )
 }
 
-export default Index
+export default CornerMenu
